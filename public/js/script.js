a55fcbcb780f276814182b08395e24a41333174e
vonlugersbutter <ayeshaumair10@gmail.com>
2020-10-11 20:33:58 -0400

    about page

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,16 @@
 ScrollReveal().reveal('.reveal');
 
 $(function() {
-    $("#navigation").load("/nav.html");
+    $("#navigation").load("nav.html");
 });
 
 $(function() {
-    $("#footer").load("/footer.html");
+    $("#footer").load("footer.html");
 });
+
+$(function() {
+  $("#teamcards").load("teamcard.html");
+})
 
 var acc = document.getElementsByClassName("accordion");
 var i;
@@ -28,14 +32,12 @@ function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -47,7 +49,6 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -60,7 +61,6 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current control button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -70,3 +70,4 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
